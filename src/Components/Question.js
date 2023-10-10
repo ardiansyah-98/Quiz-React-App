f36f4a177ba56f47334f3e3318229391382f56be
ardiansyah-98 <syahsyah9898@gmail.com>
2023-10-10 18:56:08 +0800

    Update Question.js

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,34 +1,29 @@
-// Question.js
- 
 import React, {Component} from "react";
-import Options from "./Questionopt";
+
  
 class Question extends Component{
     render() {
-        const {question, selectedOption, onOptionChange, onSubmit} = this.props;
+        const {currentQuestionIndex, handleOptionClick, calculateResultClass, QnA,onPrevQuestion,onNextQuestion} = this.props;
  
         return(
-            <div className="Question">
-                <h3>Question {question.id}</h3>
-                <h5 className="mt-2">{question.question}</h5>
-                <form onSubmit={onSubmit} className="mt-2 mb-2">
-                    <Options
-                        options={question.options}
-                        selectedOption={selectedOption}
-                        onOptionChange={onOptionChange}
-                    />
-                    <button type="submit" className="btn btn-primary m-2 ">
-                        Previous
-                    </button> 
-                    <button type="submit" className="btn btn-primary m-4">
-                        Choose
-                    </button>
-                    <button type="submit" className="btn btn-primary m-2">
-                        Next
-                    </button>
-                </form>
-                 
-            </div>
+            <div>
+            <h2>Question {currentQuestionIndex + 1}</h2>
+            <p>{QnA[currentQuestionIndex].question}</p>
+            <ul>
+              {QnA[currentQuestionIndex].options.map((option, index) => (
+                <li
+                  key={index}
+                  className={calculateResultClass(option)}
+                  onClick={() => handleOptionClick(option)}
+                >
+                  {option}
+                </li>
+                
+              ))}
+            </ul>
+          </div>
+
+          
         )
     }
 }
