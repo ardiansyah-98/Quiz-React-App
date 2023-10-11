e84ff6803b771df1630cef2a97c797634fcb5776
ardiansyah-98 <syahsyah9898@gmail.com>
2023-10-11 14:30:26 +0800

    Add sound folder and modify app.js
    Added a sound effect for both right and wrong answer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import QnA from "./Components/QuestionData";
 import Score from "./Components/Score";
-import React, { useState } from 'react';
+import React, { useState , useRef} from 'react';
 import './App.css';
 import Question from "./Components/Question";
+import correctSound from './Sounds/correct.mp3'; // Import the correct sound effect
+import wrongSound from './Sounds/wrong.mp3'; // Import the wrong sound effect
+
 
 function App() {
   
-  
+  const audioRef = useRef();
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedOption, setSelectedOption] = useState(null);
   const [currentScore, setScore] = useState(0);
   const [showResults, setShowResults] = useState(false);
   const [quizEnd,setQuizEnd]= useState(false);
-  const [reset,Quizreset]= useState(false);
+  //const [reset,Quizreset]= useState(false);
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
@@ -56,17 +59,27 @@ function App() {
   };
 
  var checkAnswer = (option) => {
- 
-    if (option === QnA[currentQuestionIndex].answer) {
-      setScore(currentScore + 1);        
+  var isCorrect= option === QnA[currentQuestionIndex].answer;
+    if (isCorrect) {
+      setScore(currentScore + 1);
+      playSoundEffect(isCorrect);        
       return currentScore;
+    }else{
+      playSoundEffect(!1);
     }
          };
+
+  const playSoundEffect = (isCorrect) => {
+  
+    const soundToPlay = isCorrect ? correctSound : wrongSound;
+    audioRef.current.src = soundToPlay;
+    audioRef.current.play();
+  };
 
   const resetQuiz = () => {
     setCurrentQuestionIndex(0);
     setScore(0);
-    Quizreset(true);
+    //Quizreset(true);
     console.log(currentQuestionIndex);
     window.location.reload(true);
           
@@ -83,7 +96,7 @@ function App() {
     <div className="App">
       <h1>Quiz App</h1>
       <div className="question-container">
-
+      <audio ref={audioRef}></audio>
         {quizEnd ? (
                   
                   <Score
@@ -95,7 +108,6 @@ function App() {
 
                     />
         ) : ( <Question
-            
                 currentQuestionIndex={currentQuestionIndex}
                 handleOptionClick={handleOptionClick}
                 calculateResultClass={calculateResultClass}
